74ac3573ae5a60a99145e75c2048c79269d2a828
James Rossini <james.rossini@posteo.de>
2026-02-05 09:14:52 +0100

    Package "read/hlp": A couple of new helper interfaces.
    These are not really integrated yet (in fact, they are partially redundant
    at the moment). This commit is preparation work for the commit(s) to come.

---
+++ src/main/java/org/gnucash/api/read/hlp/GnuCashVendor_Invc_BF.java
@@ -0,0 +1,43 @@
+package org.gnucash.api.read.hlp;
+
+import org.apache.commons.numbers.fraction.BigFraction;
+import org.gnucash.api.read.GnuCashGenerInvoice;
+
+public interface GnuCashVendor_Invc_BF {
+    
+    /**
+     * @param readVar 
+     * @return the sum of payments for invoices to this vendor
+     */
+    BigFraction getExpensesGeneratedRat(GnuCashGenerInvoice.ReadVariant readVar);
+
+    /**
+     * @return the sum of payments for invoices to this vendor
+     */
+    BigFraction getExpensesGeneratedRat_direct();
+
+    /**
+     * @return the sum of payments for invoices to this vendor
+     */
+    BigFraction getExpensesGeneratedRat_viaAllJobs();
+
+    // -------------------------------------
+
+    /**
+     * @param readVar 
+     * @return the sum of left to pay Unpaid invoiced
+     */
+    BigFraction getOutstandingValueRat(GnuCashGenerInvoice.ReadVariant readVar);
+
+    /**
+     * @return the sum of left to pay Unpaid invoiced
+     *  
+     */
+    BigFraction getOutstandingValueRat_direct();
+
+    /**
+     * @return the sum of left to pay Unpaid invoiced
+     */
+    BigFraction getOutstandingValueRat_viaAllJobs();
+
+}
---
+++ src/main/java/org/gnucash/api/read/hlp/GnuCashVendor_Invc_FP.java
@@ -0,0 +1,44 @@
+package org.gnucash.api.read.hlp;
+
+import org.gnucash.api.read.GnuCashGenerInvoice;
+
+import xyz.schnorxoborx.base.numbers.FixedPointNumber;
+
+public interface GnuCashVendor_Invc_FP {
+    
+    /**
+     * @param readVar 
+     * @return the sum of payments for invoices to this vendor
+     */
+    FixedPointNumber getExpensesGenerated(GnuCashGenerInvoice.ReadVariant readVar);
+
+    /**
+     * @return the sum of payments for invoices to this vendor
+     */
+    FixedPointNumber getExpensesGenerated_direct();
+
+    /**
+     * @return the sum of payments for invoices to this vendor
+     */
+    FixedPointNumber getExpensesGenerated_viaAllJobs();
+
+    // -------------------------------------
+
+    /**
+     * @param readVar 
+     * @return the sum of left to pay Unpaid invoiced
+     */
+    FixedPointNumber getOutstandingValue(GnuCashGenerInvoice.ReadVariant readVar);
+
+    /**
+     * @return the sum of left to pay Unpaid invoiced
+     *  
+     */
+    FixedPointNumber getOutstandingValue_direct();
+
+    /**
+     * @return the sum of left to pay Unpaid invoiced
+     */
+    FixedPointNumber getOutstandingValue_viaAllJobs();
+
+}
